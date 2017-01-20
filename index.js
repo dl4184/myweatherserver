cec232c5858906c8a7e4ca64d2b4f5e5c18147fc
dl4184 <dl4184@student.uni-lj.si>
2017-01-20 17:40:04 +0100

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var http = require('http');
+var express = require('express');
+var app = express();
+var braintree = require("braintree");
+
+
+
+
+// Hello World
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+
+
+
+
+
+//configurations
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "ysrjqnrbr479fcn2",
+  publicKey: "2w9wgg6b358yx3vg",
+  privateKey: "17dacd6bfdced23ce41d8364ad1324b3"
+});
+
+//generating token
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
